6831ed834dfe0f2b3cbf1c0c66d9f059a516aadb
Hannah Moon <hkmoon@ucdavis.edu>
2021-04-27 23:49:58 -0700

    mad libs

--- des157/madlibs/script.js
+++ des157/madlibs/script.js
@@ -21,19 +21,23 @@
           const adj3=document.querySelector('#adj3').value;
           const beverage=document.querySelector('#beverage').value;
           const adverb=document.querySelector('#adverb').value;
+          const myArray =[name, noun1, noun2, noun3, verb, adj1, adj2, adj3, beverage, adverb];
+          myArray.forEach.color="red";
           document.getElementById(`myform`).className = 'overlay hidden';
           document.getElementById(`madlib`).className = 'overlay showing';
   
           let myText;
-  
+        
           if(name && noun1 && noun2 && noun3 && adj1 && adj2 && adj3&& verb && beverage && adverb){
-              myText = `Hello ${name}! Spring is a time of renewal, rebirth, and ${noun1}. You should plant a ${noun2}. Summer is the warmest time of year. ${verb} to the beach. It would be so ${adj1}. Autumn is when the leaves ${adverb} fall from the trees. Halloween is celebrated and you will dress upas a ${noun3}. Winter is cold and ${adj2}. You can drink some hot ${beverage} , and open ${adj3} presents.`;
+              myText = `Hello ${name}! Spring is a time of renewal, rebirth, and ${noun1}. You should plant a ${noun2}. Summer is the warmest time of year, which is the perfect time to ${verb} to the beach. It would be so ${adj1}. Autumn is when the leaves ${adverb} fall from the trees. Halloween is celebrated and you will dress up as a ${noun3}. Winter is cold and ${adj2}. You can drink some hot ${beverage}, and open ${adj3} presents.`;
+              
+             
           }else{
               myText = "Please give me words so I can make your Mad Lib!";
           }
   
           
           madlib.innerHTML=myText;
-          myText.style.backgroundColor ="red";
       });
+
   })();
